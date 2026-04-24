d248a73dde3a981d30b6b7966570284c9198c1ef
TroncCommun-Duramen <contact.tronccommun@gmail.com>
2026-04-24 06:24:26 +0200

    fix(pwa): passer le cache en duramen-v3 et enregistrer sw.js dans index.html

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'duramen-v3';
+
+const FICHIERS = [
+  './',
+  './index.html',
+  './ui.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FICHIERS);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
